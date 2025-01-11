4f6ffa499864a6008b1a215051d23786de1b2aa3
KEVIN01306 <kevin60sanchez31@gmail.com>
2025-01-10 18:33:51 -0600

    commit-calse 2

--- clase 2/sentencias de control.py
+++ clase 2/sentencias de control.py
@@ -22,10 +22,26 @@ else :
 """
 
 
+
 a = 12 
 
 b = 10 
 
 c = 15
 
+
+#while = mientras
+
+control = 12 
+
+# breack sirve para salir de un bucle
+
+contador = 0
+
+control = 10
+
+palabra = "te amooo"
+
+# printear 5 veces la variable palabra, para salir utiliza el breack y como control usa la
+# variable control, utiliza if
 
