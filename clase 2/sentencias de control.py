8b7601c62f550d727a17e484bb3c2ec7420bbc1b
Andrea17-cloud <andreajimenajeronimoa@gmail.com>
2025-01-10 18:07:21 -0600

    operadores
    Holi

--- clase 2/sentencias de control.py
+++ clase 2/sentencias de control.py
@@ -24,8 +24,47 @@ else :
 
 a = 12 
 
-b = 10 
+b = 12 
 
-c = 15
+c = 12
 
+if a>b and a>c:
+    print("a es mayor que b y c")
 
+elif a<b and a<c:
+    print("a es menor que b y c")
+
+elif a>b and a<c:
+    print("a es mayor que b y menor que c")
+
+elif a<b and a>c:
+    print("a es menor que b y mayor que c")
+
+elif b>a and b>c:
+    print("b es mayor que a y c")
+
+elif b<a and b<c:
+    print("b es menor que a y c")
+
+elif b>a and b<c:
+    print("b es mayor que a y menor que c")
+
+elif b<a and b>c:
+    print("b es menor que a y mayor que c")
+
+elif c>a and c>b:
+    print("c es mayor que a y b")
+
+elif c<a and c<b:
+    print("c es menor que a y que b")
+
+elif c>a and c<b:
+    print("c es mayor que a y menor que b")
+
+elif c<a and c>b:
+    print("c es menor que a y  mayor que b")
+
+else:
+    print("a, b y c son iguales")
+
+    
